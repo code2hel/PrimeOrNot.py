05478c8b1b580f1a83050c30cfcabebbbf322cbf
CODE2HELL <73035735+code2hel@users.noreply.github.com>
2020-10-29 19:44:17 +0530

    find prime number or not
    updated

--- isPrime.py
+++ isPrime.py
@@ -1,12 +1,23 @@
-def is_prime(x):
-    if  x <= 1:
-        return False
-    n = 2
-    while n <= x/2:
-        if x % n == 0:
-            return False
-        n += 1
-    else:
-        return True
-        
-print is_prime(2200112012)
+# Program to check if a number is prime or not
+
+num = 407
+
+# To take input from the user
+#num = int(input("Enter a number: "))
+
+# prime numbers are greater than 1
+if num > 1:
+   # check for factors
+   for i in range(2,num):
+       if (num % i) == 0:
+           print(num,"is not a prime number")
+           print(i,"times",num//i,"is",num)
+           break
+   else:
+       print(num,"is a prime number")
+       
+# if input number is less than
+# or equal to 1, it is not prime
+else:
+   print(num,"is not a prime number")
+
